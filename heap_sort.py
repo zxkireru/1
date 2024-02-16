f789d289688791d6fce2a8272e961f08810e6168
zxkireru <denismeldion@gmail.com>
2024-02-17 02:20:11 +0300

    Create heap_sort.py

---
+++ heap_sort.py
@@ -0,0 +1,35 @@
+import time
+import random
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+random_list = [random.randint(0, 999999) for _ in range(50000000)]
+
+start_time = time.time()
+heap_sort(random_list)
+end_time = time.time()
+
+print(f"The pyramid sorting took {end_time - start_time} sec")
